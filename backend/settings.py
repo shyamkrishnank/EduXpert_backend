d70ce5998f440fd844dcb89039273736feb52a55
shyamkrishnank <shyamkrishnank18@gmail.com>
2024-02-02 15:56:37 +0530

    first try to connect websocket

--- backend/settings.py
+++ backend/settings.py
@@ -127,8 +127,8 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            # "hosts": [("127.0.0.1", 6379)],
-            "hosts": [("51.21.82.18", 6379)],
+            "hosts": [("127.0.0.1", 6379)],
+            # "hosts": [("51.21.82.18", 6379)],
         },
     },
 }
